187842526fe4fcf04ef212436112457b363560f7
krzysztof baranski <krisbaranski@gmail.com>
2022-12-11 22:23:29 +0100

    refactor: add Material Icon, Dialog Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,11 @@ import { GameComponent } from './game/game.component';
 import { PlayerComponent } from './player/player.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,13 +20,18 @@ import { MatIconModule } from '@angular/material';
     StartscreenComponent,
     GameComponent,
     PlayerComponent,
+    DialogAddPlayerComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
