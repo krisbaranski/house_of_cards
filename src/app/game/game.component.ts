6919ba1e274d02e44c3f0b682e69f3bd0718cbbb
krzysztof baranski <krisbaranski@gmail.com>
2022-12-14 21:41:44 +0100

    refactor: import components and material dialog

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { GameTextComponent } from '../game-text/game-text.component';
 
 @Component({
   selector: 'app-game',
