8d2c518c3f5ceb05fb74cfce7bd19de944505cf0
krzysztof baranski <krisbaranski@gmail.com>
2022-12-14 21:12:41 +0100

    refactor: add player name and player active for players elements

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent implements OnInit {
+  @Input() name;
+  @Input() playerActive: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
