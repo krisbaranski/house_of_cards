1f89cd102d993f8b723752e6da4dd306784f67b8
krzysztof baranski <krisbaranski@gmail.com>
2023-01-26 20:50:51 +0100

    refactor: import AngularFirestore, Game; correct component name; new game function

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Game } from 'src/models/game';
 
 @Component({
-  selector: 'app-startscreen',
+  selector: 'app-start-screen',
   templateUrl: './startscreen.component.html',
   styleUrls: ['./startscreen.component.scss'],
 })
-export class StartscreenComponent implements OnInit {
-  constructor(private router: Router) {}
+export class StartScreenComponent implements OnInit {
+  constructor(private firestore: AngularFirestore, private router: Router) {}
 
   ngOnInit(): void {}
 
   newGame() {
-    // start new Game
-    this.router.navigateByUrl('/game');
+    // Start game
+    let game = new Game();
+    this.firestore
+      .collection('games')
+      .add(game.toJson())
+      .then((gameInfo: any) => {
+        this.router.navigateByUrl('/game/' + gameInfo.id);
+      });
   }
 }
