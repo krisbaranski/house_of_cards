f759ac27174dde947c844982da5f9b64dafca712
krzysztof baranski <krisbaranski@gmail.com>
2023-01-26 21:20:19 +0100

    refactor: edit file to comunicate with firebase, add editPlayer and saveGame functions

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -12,40 +12,96 @@ import { EditPlayerComponent } from '../edit-player/edit-player.component';
   styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
-  pickCardAnimation = false;
-  currentCard?: string = '';
   game!: Game;
+  gameId!: string;
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: AngularFirestore,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.newGame();
+    this.route.params.subscribe((params) => {
+      //console.log(params['id']);
+      this.gameId = params['id'];
+      this.firestore
+        .collection('games')
+        .doc(this.gameId)
+        .valueChanges()
+        .subscribe((game: any) => {
+          console.log('Game update', game);
+          this.game.currentPlayer = game.currentPlayer;
+          this.game.playedCards = game.playedCards;
+          this.game.players = game.players;
+          this.game.player_images = game.player_images;
+          this.game.stack = game.stack;
+          this.game.pickCardAnimation = game.pickCardAnimtation;
+          this.game.currentCard = game.currentCard;
+        });
+    });
   }
 
   newGame() {
     this.game = new Game();
-    console.log(this.game);
   }
 
   pickCard() {
-    if (!this.pickCardAnimation) {
-      this.currentCard = this.game.stack.pop();
-      this.pickCardAnimation = true;
+    if (this.game.players.length > 1) {
+      if (!this.game.pickCardAnimation) {
+        // pop gibt letzten wert aus array & entfernt es aus dem array
+        this.game.currentCard = this.game.stack.pop()!;
+        this.game.pickCardAnimation = true;
 
-      setTimeout(() => {
-        this.game.playedCards.push(this.currentCard || '');
-        this.pickCardAnimation = false;
-      }, 1000);
+        this.game.currentPlayer++;
+        this.game.currentPlayer =
+          this.game.currentPlayer % this.game.players.length;
+
+        this.saveGame();
+
+        setTimeout(() => {
+          this.game.playedCards.push(this.game.currentCard);
+          this.game.pickCardAnimation = false;
+          this.saveGame();
+        }, 1000);
+      }
     }
+  }
+
+  editPlayer(playerId: number) {
+    console.log('Edit player', playerId);
+    const dialogRef = this.dialog.open(EditPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((change: string) => {
+      if (change) {
+        if (change == 'DELETE') {
+        } else {
+          console.log('Received change', change);
+          this.game.player_images[playerId] = change;
+          this.saveGame();
+        }
+      }
+    });
   }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogAddPlayerComponent);
 
-    dialogRef.afterClosed().subscribe((name) => {
+    dialogRef.afterClosed().subscribe((name: string) => {
       if (name && name.length > 0) {
+        // check ob die variable name existiert und wenn sie existiert ob sie größer als 0 ist
         this.game.players.push(name);
+        this.game.player_images.push('m1.png');
+        this.saveGame();
       }
     });
+  }
+
+  saveGame() {
+    this.firestore
+      .collection('games')
+      .doc(this.gameId)
+      .update(this.game.toJson());
   }
 }
