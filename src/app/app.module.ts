6fe46b98cde5138031183e5cbd6674b017a60c04
krzysztof baranski <krisbaranski@gmail.com>
2023-01-26 20:23:07 +0100

    fix: bug -  correct components name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StartscreenComponent } from './startscreen/startscreen.component';
+import { StartScreenComponent } from './startscreen/startscreen.component';
 import { GameComponent } from './game/game.component';
 import { PlayerComponent } from './player/player.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
