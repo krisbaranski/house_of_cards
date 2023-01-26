53a1ac205dc5c93fcc751775c137ff87929ad272
krzysztof baranski <krisbaranski@gmail.com>
2023-01-26 20:43:57 +0100

    refactor: add firebase data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCkywGw6NL_on259gyy__oqSZRoSwW1jPc',
+    authDomain: 'houseofcards-87806.firebaseapp.com',
+    projectId: 'houseofcards-87806',
+    storageBucket: 'houseofcards-87806.appspot.com',
+    messagingSenderId: '756004794155',
+    appId: '1:756004794155:web:0225171ed3cc40ff15df6e',
+    measurementId: 'G-C39KNEJC62',
+  },
 };
 
 /*
